3810dcff387e012a86c2bb2e6523176998cfd986
ArturTomasi <tomasi.artur@gmail.com>
2019-09-08 14:33:17 -0300

    FT: alterando layout

--- src/app/middlewares/security_manager.js
+++ src/app/middlewares/security_manager.js
@@ -1,20 +1,23 @@
 class SecurityManager {
-    hasPermission(req, res, next) {
-        if (req.header('APP_SECRET') === process.env.APP_SECRET) {
-            next();
-        }
-        else {
-            res.status('401').json({ message: 'Sem acesso ai API!' });
-        }
+  hasPermission(req, res, next) {
+    if (req.header("APP_SECRET") === process.env.APP_SECRET) {
+      next();
+    } else {
+      res.status("401").json({
+        message: "Sem acesso ai API!",
+        key: req.header("APP_SECRET"),
+        process: process.env.APP_SECRET
+      });
     }
+  }
 
-    checkId(req, res, next) {
-        if (req.params.id) {
-            next();
-        } else {
-            res.status(400).json({ message: 'Hey body, you missing one param!' });
-        }
+  checkId(req, res, next) {
+    if (req.params.id) {
+      next();
+    } else {
+      res.status(400).json({ message: "Hey body, you missing one param!" });
     }
+  }
 }
 
 module.exports = new SecurityManager();
